dd5b51e1f99e96ece17b73ba56b9b6d8396c31ae
MohammadAhmerMalick <mohammadahmermalick@gmail.com>
2025-01-09 18:06:52 +0400

    chore: style update

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-"use client"
+'use client'
 
-import Image from "next/image"
-import { useEffect, useState, type FormEvent } from "react"
+import Image from 'next/image'
+import { useEffect, useState, type FormEvent } from 'react'
 
 import getlistAction, {
   type getlistActionResponse,
-} from "@/actions/getlistAction"
-import uploadAction from "@/actions/uploadAction"
-import deleteAction from "@/actions/deleteAction"
+} from '@/actions/getlistAction'
+import uploadAction from '@/actions/uploadAction'
+import deleteAction from '@/actions/deleteAction'
 
 export default function Home() {
   const [files, setFiles] = useState<getlistActionResponse[]>([])
@@ -31,76 +31,82 @@ export default function Home() {
   useEffect(() => {
     getUploadedFiles()
   }, [])
-  console.log(files)
+
   return (
-    <div>
-      <form onSubmit={onSubmit}>
+    <div className="space-y-2 p-2 text-center">
+      <form onSubmit={onSubmit} className="flex items-center gap-2">
         <input
           required
           id="file"
           type="file"
           name="file"
           accept="image/*,video/*"
+          className="block w-full cursor-pointer rounded-lg border border-gray-600 bg-gray-700 text-gray-400 placeholder-gray-400 focus:outline-none"
         />
-        <br />
-        <button>Upload</button>
+
+        <button
+          type="button"
+          className="w-32 rounded-lg bg-blue-600 px-3 py-1.5 text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-800"
+        >
+          Upload
+        </button>
       </form>
 
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 ">
-          <thead className=" text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <table className="w-full text-sm text-gray-400">
+          <thead className="bg-gray-700 text-gray-400">
             <tr>
-              <th className="px-6 py-3">Image</th>
-              <th className="px-6 py-3">Size(px)</th>
-              <th className="px-6 py-3">Created At</th>
-              <th className="px-6 py-3">Format</th>
-              <th className="px-6 py-3 text-center">Action</th>
+              <th className="p-1">Image</th>
+              <th className="p-1">Size(px)</th>
+              <th className="p-1">Format</th>
+              <th className="p-1">Created At</th>
+              <th className="p-1">Action</th>
             </tr>
           </thead>
           <tbody>
             {files.map(
               ({
-                public_id,
-                secure_url,
                 width,
                 height,
-                created_at,
                 format,
+                public_id,
+                created_at,
+                secure_url,
               }) => (
                 <tr
                   key={public_id}
-                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                  className="border-b border-gray-700 bg-gray-800 hover:bg-gray-600"
                 >
-                  <td className="px-6 py-3">
+                  <td className="p-1">
                     <Image
                       src={secure_url}
                       width={300}
                       height={40}
                       alt={public_id}
-                      className="h-6 w-min object-contain"
+                      className="mx-auto h-6 w-min object-contain"
                     />
                   </td>
-                  <td className="px-6 py-3">
+                  <td className="p-1">
                     {width}x{height}
                   </td>
-                  <td className="px-6 py-3">{format}</td>
-                  <td className="px-6 py-3">
-                    {new Date(created_at || "").toLocaleTimeString()}{" "}
-                    {new Date(created_at || "").toLocaleDateString()}
+                  <td className="p-1">{format}</td>
+                  <td className="p-1">
+                    {new Date(created_at || '').toLocaleTimeString()}{' '}
+                    {new Date(created_at || '').toLocaleDateString()}
                   </td>
-                  <td className="px-6 py-3 text-center">
+                  <td className="p-1 text-center md:space-x-2">
                     <a
-                      className="inline-block text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded text-sm px-5 py-1 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-900"
+                      className="inline-block rounded border border-blue-500 px-5 py-1 text-blue-500 duration-150 hover:bg-blue-600 hover:text-white focus:ring-blue-900"
                       href={secure_url}
                       target="_blank"
                       rel="noopener noreferrer"
                     >
-                      Open in new tab
+                      Open
                     </a>
                     <button
                       onClick={() => deleteFile(public_id)}
                       type="button"
-                      className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded text-sm px-5 py-1 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+                      className="rounded border border-red-500 px-5 py-1 text-red-500 duration-150 hover:bg-red-600 hover:text-white focus:ring-red-900"
                     >
                       Delete
                     </button>
